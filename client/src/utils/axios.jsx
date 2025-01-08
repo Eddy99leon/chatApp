cf569760454e9cf6e3223fb7875d2f2e6de6a2bd
eddy <leoneddy504@mail.com>
2025-01-08 09:10:20 -0800

    update base url

--- client/src/utils/axios.jsx
+++ client/src/utils/axios.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: "http://localhost:3000/",
+  // baseURL: "http://localhost:3000/",
+  baseURL: "https://chatapp-fhi6.onrender.com/",
 });
